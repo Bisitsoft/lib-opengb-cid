d21ef7158bb7d32f7eff71f475323c1921cac06e
Orange_233 <874462262@qq.com>
2022-10-29 00:49:55 +0800

    Will delete somm mechanisms.

--- src/opengbex.c
+++ src/opengbex.c
@@ -1,44 +1,170 @@
+// OpenGB Exception
+
 #include "opengbex.h"
 
+#include <stdlib.h>
+#include <string.h>
+//#include <stdarg.h>
+#include <ctype.h>
+
+#include "moretype.h"
+#include "utility.h"
+
+// `size_t` is unnecessary here (`i` in each function), `int` is appropriate and enough.
+
 int lastErrorCode = OPENGB_NO_ERROR;
+
+int num_length(int num);
 
 int opengb_GetLastOpenGBErrorCode() {
     return lastErrorCode;
 }
 
 void opengb_SetLastOpenGBErrorCode(int errorCode) {
+	//opengb_CleanLastOpenGBErrorCode();
     lastErrorCode = errorCode;
 }
 
-#ifdef OPENGB_EX_COMPILE_ECTOSTR
+void opengb_CleanLastOpenGBErrorCode(){
+	lastErrorCode=OPENGB_BASE_EC_NO_ERROR;
+//#if defined(OPENGB_EX_ENABLE_EXTEND_VALUES)
+//	opengb_CleanLastOpenGBErrorExtendValues(); //<delete>Duplicate (`opengb_SetLastOpenGBErrorExtendValues` will call it, too) but necessary?</delete>Has been removed.
+//#endif
+}
+
+#if defined(OPENGB_EX_ENABLE_EXTEND_VALUES)
+OpenGBEXExtendValues *lastExtendValues=NULL;
+
+const OpenGBEXExtendValues* opengb_GetLastOpenGBErrorExtendValues() { return lastExtendValues; }
+
+void opengb_CleanLastOpenGBErrorExtendValues(){
+	int i;
+
+	if(lastExtendValues!=NULL){
+		for(i=0;i<lastExtendValues->count;++i){
+			free(lastExtendValues->values[i]);
+		}
+		free(lastExtendValues->values);
+		free(lastExtendValues);
+		lastExtendValues=NULL;
+	}
+}
+
+const char* _opengb_MakeString(const char** _str){
+	char *r = (char*)malloc(sizeof(char)*(strlen(_str)+1));
+	strcpy(r,_str);
+	return r;
+}
+
+OpenGBEXExtendValues* opengb_MakeOpenGBErrorExtendValues(const char** args, const int argc){
+	int i;
+	OpenGBEXExtendValues *r=(OpenGBEXExtendValues*)malloc(sizeof(OpenGBEXExtendValues));
+
+	r->count=argc;
+	r->values=(const char**)malloc(sizeof(const char*)*argc);
+	for (i = 0; i < argc; ++i) {
+		r->values[i]=args[i];
+	}
+	
+	return r;
+}
+
+void opengb_SetLastOpenGBErrorExtendValues(const OpenGBEXExtendValues* extendValues) {
+	opengb_CleanLastOpenGBErrorExtendValues();
+	lastExtendValues = extendValues;
+}
+
+	//右边这段代码干嘛的？ //#define OPENGB_BASE_EC_PROCESS_MSG(msg)	opengb_EXProcessMessageWithExtendValues(msg)
+void opengb_EXProcessMessageWithExtendValues(const char* msg, const char* _out_msg, const int maxSize, const OpenGBEXExtendValues const* ev){
+	int i, j, temp_idx, len = strlen(msg), arg_n;
+	int *arg_pos=NULL;
+
+	// Fast find parameter amount.
+	for(i=0,arg_n=0;i<len;++i){
+		if(msg[i]=='{'){
+			if(i+1<len && msg[i]!='{'){
+				++arg_n; //Found single '{'.
+			}else{
+				++i; //Found continuous double '{'.
+			}
+		}
+	}
+
+	if(arg_n==0){ //No parameter blank.
+		_out_msg = _opengb_MakeString(msg); //Directly return a `msg` copy.
+		return;
+	}
+	arg_pos=(int*)malloc(sizeof(int)*arg_n*2);
+
+	// Mark all parameter blank.
+	for(i=0,arg_n=0;i<len;++i){
+		if(msg[i]=='{'){
+			if(i+1<len && msg[i]!='{'){
+				++arg_n; //Found single '{'.
+			}else{
+				++i; //Found continuous double '{'.
+			}
+		}
+	}
+	
+
+	
+}
+int num_length(int num){
+	int r=1;
+	while(num>9){
+		num/=10;
+		++r;
+	}
+	return r;
+}
+#else
+void opengb_SetLastOpenGBErrorExtendValues(OpenGBEXExtendValues extendValues) { ; }
+#endif
+
+#if defined(OPENGB_EX_ENABLE_ECTOSTR)
 	#include <string.h>
 
-int opengb_ErrorCodeToString(const OPENGB_ERROR_CODE_TYPE error_code, char * const _out_msg, const int maxSize){
+//#warning Need a dictionary for large set of error messages instead switch-cases!
+
+	#define _OPENGB_SET_MSG(x) case x: msg = x##_MSG; break; //x must be a macro word name.
+
+int opengb_ErrorCodeToString(
+	const OPENGB_ERROR_CODE_TYPE error_code,
+	char * const _out_msg,
+	const int maxLen
+#if defined(OPENGB_EX_ENABLE_EXTEND_VALUES)
+	, OpenGBEXExtendValues extendValues
+#endif
+){
 	int msgLen;
 	char *msg = NULL;
 
 	switch (error_code){
-		case OPENGB_BASE_EC_NO_ERROR:
-			msg = OPENGB_BASE_EC_NO_ERROR_MSG;
-			break;
-		case OPENGB_BASE_EC_ARGUMENT_NULL:
-			msg = OPENGB_BASE_EC_ARGUMENT_NULL_MSG;
-			break;
+		_OPENGB_SET_MSG(OPENGB_BASE_EC_NO_ERROR)
 
-		case OPENGB_CID_EC_NOT_18CID:
-			msg = OPENGB_CID_EC_NOT_18CID_MSG;
-			break;
-		case OPENGB_CID_EC_CID_DATA_ERROR:
-			msg = OPENGB_CID_EC_CID_DATA_ERROR_MSG;
-			break;
+		_OPENGB_SET_MSG(OPENGB_BASE_EC_ARGUMENT_NULL)
+		_OPENGB_SET_MSG(OPENGB_BASE_EC_BUFFER_NOT_ENOUGH)
+		_OPENGB_SET_MSG(OPENGB_BASE_EC_OUT_OF_MEMORY)
+		_OPENGB_SET_MSG(OPENGB_BASE_EC_FORMAT_ERROR)
+
+		_OPENGB_SET_MSG(OPENGB_BASE_EC_SEE_ERRNO)
+		
+		_OPENGB_SET_MSG(OPENGB_CID_EC_NOT_18CID)
+		_OPENGB_SET_MSG(OPENGB_CID_EC_CID_DATA_ERROR)
 		
 		default:
 			msg = OPENGB_BASE_EC_UNKNOWN_ERROR_CODE_MSG;
 			break;
 	}
 
+	#if defined(OPENGB_EX_ENABLE_EXTEND_VALUES)
+	HERE
+	#endif
+
 	msgLen = strlen(msg);
 	if(msgLen>=maxSize){
+		_OPENGB_THROW(OPENGB_BASE_EC_BUFFER_NOT_ENOUGH); //opengbex中应当不用自己的异常机制，以免冲刷异常码，但是用户应当已经保存此处的代码，所以可以使用。
 		return -1;
 	}else{
 		strcpy(_out_msg, msg);
--- src/opengbex.h
+++ src/opengbex.h
@@ -1,55 +1,106 @@
 // OpenGB Exception
 
-#if !defined(__opengbex_h_)
+#ifndef __opengbex_h_
 	#define __opengbex_h_
 	#include "opengb_cid_switches.h"
 
 
 
 	#define OPENGB_ERROR_CODE_TYPE unsigned int
-	#define OPENGB_EC_MAKE(label, ec) (label|ec)
-	#define OPENGB_NO_ERROR 0UL
+	// Use `label`(type) as high bits (mask=0xF000) and `ec`(error code) as low bits (mask=0x0FFF) to make a complete error code.
+	#define OPENGB_EC_MAKE(label, ec) ((label)|(ec))
+	#define OPENGB_NO_ERROR 0UL //No error.
 
+	#if !defined(OPENGB_EX_ENABLE_EXTEND_VALUES)
+		#define OPENGB_BASE_EC_PROCESS_MSG(msg)	(msg) //Directly return message.
+	#else
+		#define OPENGB_BASE_EC_PROCESS_MSG(msg)	opengb_EXProcessMessageWithExtendValues(msg) //Return processed message.
+	#endif
 
+	// Each error code has its description(_MSG).
+
+	// !!!For saving memory, should use a constant string array to instead macro string. //<-----WIP!!!
 
 // === OpenGB Base Error Code Begin ==:
-	#define OPENGB_BASE_EC_LABEL 0x00000000UL
-	#define OPENGB_BASE_EC_MAKE(ec) OPENGB_EC_MAKE(OPENGB_BASE_EC_LABEL, ec)
+	#define OPENGB_BASE_EC_LABEL 0x00000000UL //Label: Base
+	#define OPENGB_BASE_EC_MAKE(ec) OPENGB_EC_MAKE((OPENGB_BASE_EC_LABEL), (ec)) //Make base error code.
+	//#define OPENGB_BASE_EC_MAY_HAS_EV 0x800000 //A flag bit: means the error message may have extend values.
 
 	#define OPENGB_BASE_EC_NO_ERROR OPENGB_NO_ERROR
 	#define OPENGB_BASE_EC_ARGUMENT_NULL OPENGB_BASE_EC_MAKE(0x000001)
+	#define OPENGB_BASE_EC_BUFFER_NOT_ENOUGH OPENGB_BASE_EC_MAKE(0x000002)
+	#define OPENGB_BASE_EC_OUT_OF_MEMORY OPENGB_BASE_EC_MAKE(0x000003)
+	#define OPENGB_BASE_EC_FORMAT_ERROR OPENGB_BASE_EC_MAKE(0x000004)
+	#define OPENGB_BASE_EC_SEE_ERRNO OPENGB_BASE_EC_MAKE(0xFFFFFF)
 	
-	#define OPENGB_BASE_EC_UNKNOWN_ERROR_CODE_MSG "Unknown error code!"
-	#define OPENGB_BASE_EC_NO_ERROR_MSG "No error."
-	#define OPENGB_BASE_EC_ARGUMENT_NULL_MSG "Argument is NULL."
+	#define OPENGB_BASE_EC_UNKNOWN_ERROR_CODE_MSG		"Unknown error code!"
+	#define OPENGB_BASE_EC_NO_ERROR_MSG					"No error."
+	#if !defined(OPENGB_EX_ENABLE_EXTEND_VALUES)
+		#define OPENGB_BASE_EC_ARGUMENT_NULL_MSG		"Argument is NULL!"
+		#define OPENGB_BASE_EC_BUFFER_NOT_ENOUGH_MSG	"The buffer you gived isn't enough. Please alloc more space to the buffer!"
+		#define OPENGB_BASE_EC_FORMAT_ERROR_MSG			"Format error."
+	#else
+		#define OPENGB_BASE_EC_ARGUMENT_NULL_MSG		"Argument \"{0}\" is NULL!"
+		#define OPENGB_BASE_EC_BUFFER_NOT_ENOUGH_MSG	"The buffer you gived isn't enough. Please alloc more space to the buffer! {0} Byte(s) memory is required."
+		#define OPENGB_BASE_EC_FORMAT_ERROR_MSG			"\"{0}\" format error."
+	#endif
+	#define OPENGB_BASE_EC_OUT_OF_MEMORY_MSG			"Out of memory!" //When the program out of memory, it shouldn't have any extend values.
+	#define OPENGB_BASE_EC_SEE_ERRNO_MSG				"errno has been set by system or other library caused OpenGB actions . Please get errno for more information."
 // :==  OpenGB Base Error Code End  ===
 
 
 
 // === OpenGB CId Error Code Begin ==:
-	#define OPENGB_CID_EC_LABEL 0x01000000UL
-	#define OPENGB_CID_EC_MAKE(ec) OPENGB_EC_MAKE(OPENGB_CID_EC_LABEL, ec)
+	#define OPENGB_CID_EC_LABEL 0x01000000UL //Label: CId
+	#define OPENGB_CID_EC_MAKE(ec) OPENGB_EC_MAKE((OPENGB_CID_EC_LABEL), (ec)) //Make CId error code.
 
 	#define OPENGB_CID_EC_NOT_18CID OPENGB_CID_EC_MAKE(0x000001)
 	#define OPENGB_CID_EC_CID_DATA_ERROR OPENGB_CID_EC_MAKE(0x000002)
 
-	#define OPENGB_CID_EC_NOT_18CID_MSG "Not a 18-digit CId!"
-	#define OPENGB_CID_EC_CID_DATA_ERROR_MSG "CitizenId data error!"
+	#define OPENGB_CID_EC_NOT_18CID_MSG					"Not a 18-digit CId!"
+	#define OPENGB_CID_EC_CID_DATA_ERROR_MSG			"CitizenId data error!"
 // :==  OpenGB CId Error Code End  ===
 
 
 
-int opengb_GetLastOpenGBErrorCode();
-void opengb_SetLastOpenGBErrorCode(OPENGB_ERROR_CODE_TYPE errorCode);
+int opengb_GetLastOpenGBErrorCode(); //Get last error code.
+//void opengb_SetLastOpenGBErrorCode(OPENGB_ERROR_CODE_TYPE errorCode);  //Get last error code.
+//void opengb_CleanLastOpenGBErrorCode();  //Clean last error code (and extend values). It will not clear extend values.
 
+	#if defined(OPENGB_EX_ENABLE_EXTEND_VALUES)
+typedef struct{
+	int count;
+	char** values;
+}OpenGBEXExtendValues;
+const OpenGBEXExtendValues* opengb_GetLastOpenGBErrorExtendValues(); //Get parameters of last error message.
+//void opengb_SetLastOpenGBErrorExtendValues(const OpenGBEXExtendValues* extendValues); //Set parameters of last error message. It will call `opengb_CleanLastOpenGBErrorCode` before set value.
+//void opengb_CleanLastOpenGBErrorCode(); //Clean parameters of last error message. It will use `free` function to release all the parameters.
+void opengb_EXProcessMessageWithExtendValues(const char* msg, const char* _out_msg, const int maxSize, const OpenGBEXExtendValues const* ev); //Process error message with ectend values.
+const char* _opengb_MakeString(const char** _str); //Return a `_str` copy contained by a new dynamic array.
+OpenGBEXExtendValues* opengb_MakeOpenGBErrorExtendValues(const char** args, const int argc); //Box `args` into a new `OpenGBEXExtendValues`.
+	#endif
+
+	// Throw error code. In fact, it just set last error code.
 	#define _OPENGB_THROW(errorCode) opengb_SetLastOpenGBErrorCode(errorCode);
-	#define _OPENGB_TRY opengb_SetLastOpenGBErrorCode(OPENGB_NO_ERROR);
-	#define _OPENGB_CATCH(errorCode) if(opengb_GetLastOpenGBErrorCode()==errorCode){
-	#define _OPENGB_CATCHX(errorCode) }else if(opengb_GetLastOpenGBErrorCode()==errorCode){
+	// Throw error code with extend values. In fact, it just set last error code with extend values.
+	// If you don't define `OPENGB_EX_ENABLE_EXTEND_VALUES`, the extend values will be dropped. 
+	#define _OPENGB_THROW_EV(errorCode, extendValues) opengb_SetLastOpenGBErrorCode(errorCode); opengb_SetLastOpenGBErrorExtendValues(extendValues);
+	#define _OPENGB_TRY opengb_CleanLastOpenGBErrorCode();
+	#define _OPENGB_CATCH(errorCode) if(opengb_GetLastOpenGBErrorCode()==(errorCode)){
+	#define _OPENGB_CATCHX(errorCode) }else if(opengb_GetLastOpenGBErrorCode()==(errorCode)){
 	#define _OPENGB_CATCH_CLOSE }
 
-	#ifdef OPENGB_EX_COMPILE_ECTOSTR
-int opengb_ErrorCodeToString(const OPENGB_ERROR_CODE_TYPE error_code, char * const _out_msg, const int maxLen); //return -1 for failure, >0 for success and length.
+	#if defined(OPENGB_EX_ENABLE_ECTOSTR)
+// Generate the error message corresponding to the error code.
+// Return -1 means failure. Return any val >0 means success and the value is the length of `_out_msg`.
+int opengb_ErrorCodeToString(
+	const OPENGB_ERROR_CODE_TYPE error_code,
+	char * const _out_msg,
+	const int maxLen
+		#if defined(OPENGB_EX_ENABLE_EXTEND_VALUES)
+	, OpenGBEXExtendValues extendValues
+		#endif
+);
 	#endif
 
 #endif
